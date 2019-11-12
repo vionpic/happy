03e4206ce1dde37f903fd3f4bdf6896e2b646082
vionpic <vionpic@gmail.com>
2019-11-12 20:48:58 +0700

    initial commit abc

--- query.js
+++ query.js
@@ -1,1 +1,20 @@
-$('audio')[0].play();
+$(document).ready(function(){
+    $("#play").click(function(){
+        $(".play").css({"display":"none"});
+        $(".subject").css({"display":"none"});
+        $(".you").css({"display":"block"});
+        $(".background").css({"display":"block"});
+        var audio = document.getElementById("audio");
+        audio.play();
+        setInterval(function(){ 
+            $(".cake").css({"display":"block"});
+        },8000);
+        setInterval(function(){ 
+            $(".happy").css({"display":"block"});
+        },18000);
+        setInterval(function(){ 
+            $(".hieuung").css({"display":"block"});
+        },20000);
+    });
+});
+
